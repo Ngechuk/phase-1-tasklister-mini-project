917c84af7fa19d105f17482d2a35b3d4853fd825
Kevin <kelvinngechu@gmail.com>
2022-12-06 14:21:34 +0300

    eventList

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  const fieldInput = document.getElementbyId("new-task-description");
+  const formEle = document.querySelector("form");
+  formEle.addEventListener('submit',(e) =>{
+    let newtask = fieldInput.value
+    e.preventDefault();
+  
+  })
+}:
